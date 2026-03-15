9dd8d981feca31c2877fb5ea1ba53cec0670eaad
Nagarajuknr <105048657+Nagarajuknr@users.noreply.github.com>
2026-03-15 14:31:27 +0530

    Add new CORS origin for production deployment

--- backend/src/main.py
+++ backend/src/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # Allow React portal (localhost:5173) to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins     = ["http://localhost:5173", "http://localhost:3000"],
+    allow_origins     = ["http://localhost:5173", "http://localhost:3000","https://hospital-locator-six.vercel.app/"],
     allow_credentials = True,
     allow_methods     = ["*"],
     allow_headers     = ["*"],
